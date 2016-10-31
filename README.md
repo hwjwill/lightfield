ad5f1067bd05727682804d05559dc6f0e5fff550
hwjwill <hwjwill@hotmail>
2016-10-31 00:10:36 -0700

    Completed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lightfield Camera
+
+As [this paper](http://graphics.stanford.edu/papers/lfcamera/lfcamera-150dpi.pdf) by Ng et al. demonstrated, capturing multiple images over a plane orthogonal to the optical axis enables achieving complex effects using very simple operations like shifting and averaging. The goal of this project is to reproduce some of these effects using real lightfield data. Fore more results: http://inst.eecs.berkeley.edu/~cs194-26/fa16/upload/files/proj6/cs194-26-acm/
+
+![Example](http://inst.eecs.berkeley.edu/~cs194-26/fa16/hw/proj6-lightfield/images/changeFocus.gif)
+![Example](http://inst.eecs.berkeley.edu/~cs194-26/fa16/hw/proj6-lightfield/images/changeAperture.gif)
+
+# To run code
+
+Please see comments in main.m for further definition of parameter
+```
+main(<Part number>, <Parameter>)
+``` 
--- main.m
+++ main.m
@@ -1,29 +1,55 @@
-function [] = main()
-part1 = true;
-if part1
+function [] = main(part, parameter)
+% Part represents the part of each assignment. For part 1, parameter is the
+% constant that multiple to displacement from 8,8'th picture. For part 2,
+% parameter is the radius around picture 8,8 to be included in averaging
+
+% Folder name is the folder that saves all pictures from Stanford Light
+% Field Archive
+folderName = 'rectified/';
+files = strcat(folderName, '*.png');
+fnames = dir(files);
+picCount = size(fnames, 1);
+route = strcat(folderName, fnames(1).name);
+im = imread(route);
+avg = zeros(size(im, 1), size(im, 2), 3);
+if part == 1
     %% Part one: Depth Refocusing
-    fnames = dir('rectified/*.png');
-    picCount = size(fnames, 1);
-    route = strcat('rectified/', fnames(1).name);
-    im = imread(route);
-    %im = imresize(im, 0.25);
-    c = -2;
-    avg = zeros(size(im, 1), size(im, 2), 3);
     for a = 1:picCount
-        route = strcat('rectified/', fnames(a).name);
+        route = strcat(folderName, fnames(a).name);
         im = double(imread(route));
-        %im = imresize(im, 0.25);
         routeArray = strsplit(fnames(a).name, '_');
         y = str2double(routeArray(2));
-        x = -str2double(routeArray(3));
-        im = circshift(im, [(8 - y) * c, (8 - x) * c]);
+        x = str2double(routeArray(3));
+        im = circshift(im, [(8 - y) * parameter, (x - 8) * parameter]);
         avg = avg + im / picCount;
     end
     result = uint8(avg);
-    filename = strcat(num2str(c), '.png');
+    filename = strcat(num2str(parameter), 'lego.png');
     imwrite(result, filename);
-    imshow(result);
-else
+elseif part == 2
     %% Part two: Aperture Adjustment
+    totalNum = (2 * parameter + 1) ^ 2;
+    for a = 1:picCount
+        route = strcat(folderName, fnames(a).name);
+        im = double(imread(route));
+        routeArray = strsplit(fnames(a).name, '_');
+        y = str2double(routeArray(2));
+        x = str2double(routeArray(3));
+        if inBoundary(x, y, parameter)
+            avg = avg + im / totalNum;
+        end
+    end
+    result = uint8(avg);
+    filename = strcat(num2str(parameter), 'legoApeature.png');
+    imwrite(result, filename);
+end
+end
+
+%% Helper functions
+function [r] = inBoundary(x, y, r)
+if x >= 8 - r && x <= 8 + r && y >= 8 - r && y <= 8 + r
+    r = true;
+else
+    r = false;
 end
 end
